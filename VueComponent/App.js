019ca6b8a3dd2e705e31cb7c1d38fd3ca6b39612
mylin95 <mylin95@126.com>
2020-01-05 11:29:20 +0800

    Vue组件极致化：单页面应用，只有一个入口

---
+++ VueComponent/App.js
@@ -0,0 +1,24 @@
+;(function() {
+    const template = `
+    <div>
+        <!--头部导航区域-->
+        <app-nav></app-nav>
+        <!--核心区域:分左右两边-->
+        <div class="container-fluid">
+            <div class="row">
+            <!--左边菜单栏区域-->
+            <app-leaf></app-leaf>
+            <!--右边主页面区域: 分上下两个区域-->
+            <app-home></app-home>
+            </div>
+        </div>
+    </div>`
+    window.App = {
+        template,
+        components: {
+            AppNav,
+            AppLeaf,
+            AppHome
+        }
+    }
+})()
---
+++ VueComponent/main.js
@@ -0,0 +1,10 @@
+// 1、AppNav组件可以定义在此处，
+      // 亦可定义在外部js文件中
+      new Vue({
+        el: '#app',
+        // template: '<app></app>',
+        template: '<app/>',
+        components: {
+          App
+        }
+      })
