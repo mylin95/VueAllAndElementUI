cc98692385d521cc8e3083c9063ed25cb1f60880
mylin95 <mylin95@126.com>
2020-01-05 17:51:00 +0800

    组件间通信：子组件向父组件传递数据

--- VueComponent/components/home/AppHome.js
+++ VueComponent/components/home/AppHome.js
@@ -3,7 +3,8 @@
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
             <!--右边上半区域-->
             <h1 class="page-header">Dashboard</h1>
-            <dashboard></dashboard>
+            <!--自定义事件：@自定义事件名="事件名"-->
+            <dashboard :hobbies="hobbies" @delete_hobby="deleteHobby"></dashboard>
             <!--右边下半区域-->
             <h2 class="sub-header">Section title</h2>
             <home-list :empList="empList" :deleteEmp="deleteEmp" ></home-list>
@@ -12,6 +13,7 @@
         template,
         data() {
             return {
+                hobbies: ['coding', '睡觉', '打豆豆', '看书'],
                 empList: [
                     {id: 1001, name: '胡歌1', salary: 10000},
                     {id: 1002, name: '胡歌2', salary: 10000},
@@ -24,6 +26,9 @@
         methods: {
             deleteEmp(index) {
                 this.empList.splice(index, 1)
+            },
+            deleteHobby(index) {
+                this.hobbies.splice(index, 1)
             }
         },
         components: {
