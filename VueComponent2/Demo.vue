1e3f9436162ca1a3449aad86c26aba84a4b8d96a
mylin95 <mylin95@126.com>
2020-01-09 10:59:00 +0800

    Vue组件单文件

---
+++ VueComponent2/Demo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-sm-3 col-md-2 sidebar">
+      <ul class="nav nav-sidebar">
+        <li class="active"><a href="#">Overview 
+        <span v-show="delNum != 0">( {{ delNum }} )</span></a></li>
+        <li><a href="#">Reports</a></li>
+        <li><a href="#">Analytics</a></li>
+        <li><a href="#">Export</a></li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        delNum: 0 // 已经删除的总数量
+        }
+    },
+    created() {
+        // 订阅消息
+        PubSub.subscribe('changeNum', (event, num) => { // 箭头函数中的this，指的是它的父类
+        // 事件的回调函数
+        // 统计已经删除的函数
+        this.delNum = this.delNum + num
+        })
+    },
+}
+</script>
+
+<style scoped>
+/* 组件自定义样式 */
+</style>
